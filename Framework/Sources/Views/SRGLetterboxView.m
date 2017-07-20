6d46c8afd6b24401a5a980461e765bc2e8d74ae1
Samuel Défago <defagos@gmail.com>
2017-07-20 13:55:21 +0200

    Update loading indicator

--- Framework/Sources/Views/SRGLetterboxView.m
+++ Framework/Sources/Views/SRGLetterboxView.m
@@ -138,7 +138,7 @@ static void commonInit(SRGLetterboxView *self);
 {
     [super awakeFromNib];
     
-    UIImageView *loadingImageView = [UIImageView srg_loadingImageView35WithTintColor:[UIColor whiteColor]];
+    UIImageView *loadingImageView = [UIImageView srg_loadingImageView48WithTintColor:[UIColor whiteColor]];
     loadingImageView.alpha = 0.f;
     [self.mainView insertSubview:loadingImageView aboveSubview:self.playbackButton];
     [loadingImageView mas_makeConstraints:^(MASConstraintMaker *make) {
